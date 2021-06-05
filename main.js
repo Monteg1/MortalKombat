499aa70627cf4cc19322e6b0f65d884a84cc9514
Monteg1 <65545981+Monteg1@users.noreply.github.com>
2021-06-05 20:12:00 +0300

    HomeWork-3
    Вроде всё работает как нужно )

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
+let arenas = document.querySelector('.arenas');
+let randomButton = document.querySelector('.button');
+
 let player1 = {
+  player: 1,
   name: 'Scorpion',
-  hp: "100%",
+  hp: 100,
   img: 'http://reactmarathon-api.herokuapp.com/assets/scorpion.gif',
   weapon: ['Меч', 'Пистолет', 'Граната'],
   attack: function() {
@@ -8,9 +12,18 @@ let player1 = {
   }
 }
 
+function createElement(tag, className) {
+  const $tag = document.createElement(tag);
+  if (className) {
+    $tag.classList.add(className);
+  }
+   return $tag;
+}
+
 let player2 = {
+  player: 2,
   name: 'Kitana',
-  hp: "50%",
+  hp: 100,
   img: 'http://reactmarathon-api.herokuapp.com/assets/kitana.gif',
   weapon: ['Меч', 'Пистолет', 'Граната'],
   attack: function() {
@@ -19,38 +32,67 @@ let player2 = {
 }
 
 
-function createPlayer(p, n) {
+function createPlayer(nameObj) {
 
 
-  let player = document.createElement('div');
-  player.classList.add(p);
+  let player = createElement('div', 'player'+nameObj.player);
 
-  let progressBar = player.appendChild(document.createElement('div'));
-  progressBar.classList.add('progressbar')
+  let progressBar = createElement('div', 'progressbar');
+  let character = createElement('div', 'character');
+  let life = createElement('div', 'life');
+  let name = createElement('div', 'name');
+  let img = createElement('img');
+  img.src = nameObj.img
 
-  let character = player.appendChild(document.createElement('div'));
-  character.classList.add('character');
+  player.appendChild(progressBar);
+  player.appendChild(character);
 
-  let life = progressBar.appendChild(document.createElement('div'));
-  life.classList.add('life');
+  progressBar.appendChild(name);
+  progressBar.appendChild(life);
 
-  let name = progressBar.appendChild(document.createElement('div'));
-  name.classList.add('name');
-
-  let img = document.createElement('img');
-  img.src = n.img
 
   character.appendChild(img);
 
-  life.style.width = n.hp;
-  name.innerText = n.name;
+  life.style.width = nameObj.hp + "%";
+  name.innerText = nameObj.name;
 
-
-  let arenas = document.querySelector('.arenas');
-  arenas.appendChild(player);
-  
-
+  return player;  
 }
 
-createPlayer('player1', player1);
-createPlayer('player2', player2);
+function changeHP(player) {
+  let playerLife = document.querySelector('.player'+ player.player +' .life');
+    player.hp -= Math.ceil(Math.random() * 20);
+    playerLife.style.width = player.hp + "%";
+    console.log(player.hp);
+
+
+
+  if (player.hp <= 0) {
+
+    playerLife.style.width = "0%";
+    arenas.appendChild(playerLose(player.name));
+    randomButton.disabled = true;
+  }
+}
+
+function playerLose(name) {
+  let loseTittle = createElement('div', 'loseTitle');
+  if (name == player1.name) {
+    loseTittle.innerText = player2.name + " wins";
+  } else {
+    loseTittle.innerText = player1.name + " wins";
+  }
+  
+  return loseTittle;
+ }
+
+randomButton.addEventListener('click', function() {
+console.log('Клик');
+changeHP(player1);
+changeHP(player2);
+  
+});
+
+arenas.appendChild(createPlayer(player1));
+arenas.appendChild(createPlayer(player2));
+
